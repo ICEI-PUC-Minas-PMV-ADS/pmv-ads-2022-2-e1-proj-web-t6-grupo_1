de35e84bb77b24ed27da49f599b7fb381e0cdfca
Brendon Guimaraes <brendonguima@gmail.com>
2022-10-29 19:49:33 -0300

    Template

---
+++ src/noticias.js
@@ -0,0 +1,19 @@
+var db = {
+    dados: [
+        {
+            perfil: 'Geraldo Magela',
+            titulo: 'Saúde - Falta de médicos',
+            descricao: 'Bom dia Exemos Senhores, Venho por este meio humildemente falar do meu caso em relação a este problema. Eu me chamo Geraldo Magela sou logista, resido em Belo Horizon…',
+            imagem: 'https://s2.glbimg.com/YXmhCmZ5awKDsXwrHhUZDaB4hWc=/0x0:1024x768/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/p/Z/juBuIhSPyuRAgPq6astQ/whatsapp-image-2020-08-07-at-10.30.49.jpeg',
+            zona: 'Norte',
+            comentario: '2'
+        }, {
+            perfil: 'Dalva Parreiras',
+            titulo: 'Escola Estadual Presidente Dutra',
+            descricao: 'Bom dia Exemos Senhores, Venho por este meio humildemente falar do meu caso em relação a esta escola. Eu me chamo Dalva Parreiras sou manicure, resido em Belo Horizonte…',
+            imagem: 'https://www.tce.mg.gov.br/ImagemDestaque/1111623737.jpg',
+            zona: 'Leste',
+            comentario: '7'
+        }
+    ]
+}
